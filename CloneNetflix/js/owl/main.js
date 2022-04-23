b264ff6a1293686a9a0dd7a67a184370681af957
Jrm1992 <103078485+Jrm1992@users.noreply.github.com>
2022-04-23 19:35:22 -0300

    Add files via upload

--- CloneNetflix/js/owl/main.js
+++ CloneNetflix/js/owl/main.js
@@ -1,18 +1,18 @@
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:20,
-    dots:false,
-    nav:true,
-    autoWidth:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
-    }
-})
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 20,
+  dots: false,
+  nav: true,
+  autoWidth: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 5,
+    },
+  },
+});
