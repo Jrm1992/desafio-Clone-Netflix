26ef45c6d4fc8ba282f1df5fba31d2d21e2adc14
REVISION-PC\Jose Roberto <jose_roberto_6@msn.com>
2022-05-01 22:07:23 -0300

    no message

--- CloneNetflix/js/owl/main.js
+++ CloneNetflix/js/owl/main.js
@@ -2,17 +2,62 @@ $(".owl-carousel").owlCarousel({
   loop: true,
   margin: 20,
   dots: false,
-  nav: true,
-  autoWidth: true,
+  nav: false,
+  autowidth: false,
   responsive: {
     0: {
-      items: 1,
-    },
-    600: {
       items: 3,
     },
-    1000: {
+    600: {
       items: 5,
+    },
+    1000: {
+      items: 7,
     },
   },
 });
+
+function myFunction1() {
+  document.getElementById("Cavaleiro").style.display = "flex";
+  document.getElementById("Vikings").style.display = "none";
+  document.getElementById("Halo").style.display = "none";
+  document.getElementById("TWD").style.display = "none";
+  document.getElementById("Peaky").style.display = "none";
+}
+function myFunction2() {
+  document.getElementById("Cavaleiro").style.display = "none";
+  document.getElementById("Vikings").style.display = "none";
+  document.getElementById("Halo").style.display = "flex";
+  document.getElementById("TWD").style.display = "none";
+  document.getElementById("Peaky").style.display = "none";
+}
+function myFunction3() {
+  document.getElementById("Cavaleiro").style.display = "none";
+  document.getElementById("Vikings").style.display = "none";
+  document.getElementById("Halo").style.display = "none";
+  document.getElementById("TWD").style.display = "flex";
+  document.getElementById("Peaky").style.display = "none";
+}
+function myFunction4() {
+  document.getElementById("Cavaleiro").style.display = "none";
+  document.getElementById("Vikings").style.display = "flex";
+  document.getElementById("Halo").style.display = "none";
+  document.getElementById("TWD").style.display = "none";
+  document.getElementById("Peaky").style.display = "none";
+}
+function myFunction5() {
+  document.getElementById("Cavaleiro").style.display = "none";
+  document.getElementById("Vikings").style.display = "none";
+  document.getElementById("Halo").style.display = "none";
+  document.getElementById("TWD").style.display = "none";
+  document.getElementById("Peaky").style.display = "flex";
+}
+function bigImg(x) {
+  x.style.height = "270px";
+  x.style.width = "170px";
+}
+
+function normalImg(x) {
+  x.style.height = "250px";
+  x.style.width = "156px";
+}
---
+++ Projeto2/js/main.js
@@ -0,0 +1,9 @@
+$('.slider-1').slick({
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: true,
+    Infinity: true,
+    dots:true,
+    fade: true,
+    cssEase: 'linear'
+});
